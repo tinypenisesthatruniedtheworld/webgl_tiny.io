2bff9d141690c99d8b11d3e677afc512629f1312
Tiny Penises That Ruined The World <tinypenisesthatruniedtheworld@gmail.com>
2021-11-30 12:51:59 -0600

    tiny.io WebGL version 1
    - tiny.io WebGL version 1
    - Blockchain stuff not implemented yet

---
+++ Template/app.js
@@ -0,0 +1,54 @@
+(function () {
+
+    var container = document.querySelector("#unity-container");
+    var canvas = document.querySelector("#unity-canvas");
+    var loader= document.querySelector("#loader");
+    var loaderFill= document.querySelector("#fill");
+
+    function onProgress(progress) {
+        loaderFill.style.width = progress * 100 + "%";
+    }
+
+    function onComplete(unityInstance) {
+        loader.remove();
+    }
+    function onWindowResize() {
+        var width = window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
+
+        var height = window.innerHeight
+        || document.documentElement.clientHeight
+        || document.body.clientHeight;
+
+        canvas.height=height;
+        canvas.width=width;
+    }
+
+    var buildUrl = "Build";
+    var loaderUrl = buildUrl + "/Tiny_Web_Project.loader.js";
+    var config = {
+        dataUrl: buildUrl + "/Tiny_Web_Project.data",
+        frameworkUrl: buildUrl + "/Tiny_Web_Project.framework.js",
+        codeUrl: buildUrl + "/Tiny_Web_Project.wasm",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "Powerful Men With Tiny Penises That Ruined The World",
+        productName: "tiny.io",
+        productVersion: "",
+    };
+
+    var script = document.createElement("script");
+    script.src = loaderUrl;
+    script.onload = () => {
+        createUnityInstance(canvas, config, onProgress)
+            .then(onComplete)
+            .catch((message) => {
+                alert(message);
+        });
+    };
+    document.body.appendChild(script);
+
+    window.addEventListener('resize', onWindowResize);
+    onWindowResize();
+
+})();
